37444f8d2e746b609ea3e43d5d34abff2107052e
DawidWilk <dawid.wilu@gmail.com>
2015-12-11 23:01:12 +0100

    Update k_dodaj.php

--- k_dodaj.php
+++ k_dodaj.php
@@ -1,4 +1,4 @@
-<form name="form-dodawanie" action="" method="post" id="596398803">
+4<form name="form-dodawanie" action="" method="post" id="596398803">
           <center>Operacja dodawania detalu:</center><br><br>
           <center>Nazwa detalu: <input type="text" name="nowy_detal_nazwa"> </center>
           <center><input type="submit" name="detal_dodaj" value="Dodaj"> </center>
@@ -13,22 +13,26 @@ if (isset($_POST['detal_dodaj']))
         $zapytanie = mysql_query ($question);   //dodanie detalu
         //echo $question;
         
-        //dorobić pobranie id usera,
-        //pobranie id dodanego detalu
-        //wpisanie id dodanego detalu do zapytania historii
-        //wpisanie id usera do historii
+        $question = 'SELECT id_detal, nazwa FROM system_detale.detal WHERE nazwa=\''.$_POST['nowy_detal_nazwa'].'\';';
+        $res=mysql_query($question);    //pobranie id detalu
+                while ($baza=mysql_fetch_array($res))
+                {
+                    $local_detal_id = $baza['id_detal'];
+                    $nazwa_detalu_temp = $baza['nazwa'];
+                } 
                 
-        $question = 'INSERT INTO system_detale.historia (operatorzy_id_operatorzy, detal_id_detal, maszyny_id_maszyny, typ_operacji, maszyna_poprzednia, data) VALUES (15,9,0,"dodanie",0,"'.date('H:i:s Y-m-d').'");';
+        $question = 'INSERT INTO system_detale.historia (operatorzy_id_operatorzy, detal_id_detal, maszyny_id_maszyny, typ_operacji, maszyna_poprzednia, data) VALUES ('.$SESSION ['osoba_id'].','.$local_detal_id.',0,"dodanie",0,"'.date('H:i:s Y-m-d').'");';
         $zapytanie_historia = mysql_query($question);  //wpis do historii
         //echo $question;
 
         if (( $zapytanie === \TRUE)&& ( $zapytanie_historia === \TRUE))
             {
-                echo "Pomyślnie dodano detal"; 
-                //header('Refresh: 1; url=panel_operatora.php'); //odświez stronę
+                echo "Pomyślnie dodano detal:"; echo "<br>";
+                echo "id: $local_detal_id, nazwa: $nazwa_detalu_temp";
             } 
         else 
             {echo "Błąd sql";}
     }
 }
 ?>
+
