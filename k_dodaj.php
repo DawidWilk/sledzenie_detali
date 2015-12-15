86fc5e5389a86c76b61d4d4db5a91b24a2a87fc7
DawidWilk <dawid.wilu@gmail.com>
2015-12-15 12:29:53 +0100

    Update k_dodaj.php

--- k_dodaj.php
+++ k_dodaj.php
@@ -9,16 +9,16 @@ if (isset($_POST['detal_dodaj']))
 {
     if (!empty($_POST['nowy_detal_nazwa']))  //jeżeli nazwa nowego detalu jest wpisana
     {
-        $question = 'INSERT INTO system_detale.detal (nazwa, maszyny_id_maszyny) VALUES ("'.$_POST['nowy_detal_nazwa'].'",0);';
+        $question = 'INSERT INTO system_detale.detal (nazwa_detalu, maszyny_id_maszyny, stan) VALUES ("'.$_POST['nowy_detal_nazwa'].'",0,"uzycie");';
         $zapytanie = mysql_query ($question);   //dodanie detalu
         //echo $question;
         
-        $question = 'SELECT id_detal, nazwa FROM system_detale.detal WHERE nazwa=\''.$_POST['nowy_detal_nazwa'].'\';';
+        $question = 'SELECT id_detal, nazwa_detalu FROM system_detale.detal WHERE nazwa_detalu=\''.$_POST['nowy_detal_nazwa'].'\';';
         $res=mysql_query($question);    //pobranie id detalu
                 while ($baza=mysql_fetch_array($res))
                 {
                     $local_detal_id = $baza['id_detal'];
-                    $nazwa_detalu_temp = $baza['nazwa'];
+                    $nazwa_detalu_temp = $baza['nazwa_detalu'];
                 } 
                 
         $question = 'INSERT INTO system_detale.historia (operatorzy_id_operatorzy, detal_id_detal, maszyny_id_maszyny, typ_operacji, maszyna_poprzednia, data) VALUES ('.$SESSION ['osoba_id'].','.$local_detal_id.',0,"dodanie",0,"'.date('H:i:s Y-m-d').'");';
